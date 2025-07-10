b8eaeab89eca8910833d319d83f506f2361386d5
maihunsandip <skole472@gmail.com>
2025-07-11 00:40:13 +0530

    Configured utilities for errors, response and async functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import connectDB from "./db/connectDB.js";
 import "dotenv/config";
+import app from "./app.js"
 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`server is running at ${process.env.PORT}!!`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection error:" + err);
+})
