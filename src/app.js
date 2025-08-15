e027c3a8bab300ab5aa5b4c1077da7c0165d6647
maihunsandip <skole472@gmail.com>
2025-08-15 20:41:46 +0530

    Setup is configured

--- src/app.js
+++ src/app.js
@@ -11,8 +11,15 @@ app.use(cors({
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
-app.use(cookieParser)
+app.use(cookieParser())
 
+
+//Routes import
+import userRout from "./routes/user.routes.js"
+
+
+//Routes declaration
+app.use("/api/v1/users", userRout)
 
 
 
